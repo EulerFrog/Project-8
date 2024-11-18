0254c5a0dabd27c92c2be154253633465fca88c6
Michelle Fast <michellefast8@gmail.com>
2024-11-18 14:29:38 -0800

    Clean up plotting pipeline, make functions, comments

--- plots.py
+++ plots.py
@@ -1,81 +1,68 @@
-import matplotlib.pyplot as plt 
+import matplotlib.pyplot as plt
 from PIL import Image, ImageSequence, ImageEnhance
 import tifffile as tf
-import numpy as np 
+import numpy as np
 from matplotlib.animation import FuncAnimation
 
-FILE_PATH = 'dish2.tif' 
-# GIF_FILE = 'dish2.gif'
+FILE_PATH = 'dish2_ATP_1_MMStack_Pos0.ome.tif'
 CONTRAST_LEVEL = 2.0
-SAVE_PLT = True 
+SAVE_PREFIX = None
 
-def normalization(data):
+# Min/Max Normalization
+def normalize(data):
     data_min = np.min(data)
     data_max = np.max(data)
     data_norm = (data - data_min) / (data_max - data_min)
-    return data_norm 
+    return data_norm
 
 # Parse frames
 images = tf.imread(FILE_PATH)
 images_np = []
 images_with_contrast = []
 for img in images:
-    # no contrast change 
+    # no contrast change
     image_pil = Image.fromarray(img).convert('L') # Convert to grayscale
     image_numpy = np.array(image_pil)
     images_np.append(image_numpy)
 
-    # constrast change 
+    # constrast change
     enhancer = ImageEnhance.Contrast(image_pil)
     image_enhanced = enhancer.enhance(CONTRAST_LEVEL) # Increase contrast
     image_numpy_with_contrast = np.array(image_enhanced)
     images_with_contrast.append(image_numpy_with_contrast)
 
-
+# No contrast frame averages
 img_avgs = []
 for img in images_np:
     img_avgs.append(np.average(img))
 
+# Constrast frame averages
 img_avgs_with_contrast = []
 for img in images_with_contrast:
     img_avgs_with_contrast.append(np.average(img))
 
-if not SAVE_PLT:
-    # w/ normalization, w/ contrast
-    plt.plot(normalization(img_avgs_with_contrast))
-    plt.title("Normalization and Contrast Modification")
-    plt.show()
-    # w/out normalization, w/out contrast
-    plt.plot(img_avgs)
-    plt.title("No Normalization, No Contrast Modification")
-    plt.show()
-    # w/ normalization, w/out contrast
-    plt.plot(normalization(img_avgs))
-    plt.title("Normalization and No Contrast Modification")
-    plt.show()
-    # w/out normalization, w/ contrast
-    plt.plot(img_avgs_with_contrast)
-    plt.title("No Normalization, Contrast Modification")
-    plt.show()
+fig, axs = plt.subplots(2,2,figsize=(20,10))
+fig.suptitle(f"Luminosity Over Time\n{FILE_PATH.split('.')[0].replace('_', ' ')}", fontsize=16)
 
+# Define x axis bounds (number of frames)
+x_axis_contrast = range(len(img_avgs_with_contrast))
+x_axis_no_contrast = range(len(img_avgs))
+
+# Draw subplot
+def subplot(axis, x, y, norm, contrast):
+    axis.scatter(x, y, c='seagreen')
+    axis.set_title(f"{'' if contrast else 'No'} Contrast Modification")
+    axis.set_xlabel("Frame")
+    axis.set_ylabel("Avg Luminosity (Pixel Values)")
+
+subplot(axs[0,0], x_axis_no_contrast, img_avgs, False, False) # No Norm, No Contrast
+subplot(axs[0,1], x_axis_no_contrast, normalize(img_avgs), True, False) # Norm, No Contrast
+subplot(axs[1,0], x_axis_contrast, img_avgs_with_contrast, False, True) # No Norm, Contrast
+subplot(axs[1,1], x_axis_contrast, normalize(img_avgs_with_contrast), True, True) # Norm, Contrast
+
+plt.tight_layout()
+
+if SAVE_PREFIX is None:
+    plt.show()
 else:
-    # w/ normalization, w/ contrast
-    plt.plot(normalization(img_avgs_with_contrast))
-    plt.title("Normalization and Contrast Modification")
-    plt.savefig('norm_contrast.png')
-    plt.close()
-    # w/out normalization, w/out contrast
-    plt.plot(img_avgs)
-    plt.title("No Normalization, No Contrast Modification")
-    plt.savefig('no_norm_no_contrast.png')
-    plt.close()
-    # w/ normalization, w/out contrast
-    plt.plot(normalization(img_avgs))
-    plt.title("Normalization and No Contrast Modification")
-    plt.savefig('norm_only.png')
-    plt.close()
-    # w/out normalization, w/ contrast
-    plt.plot(img_avgs_with_contrast)
-    plt.title("No Normalization, Contrast Modification")
-    plt.savefig('contrast_only.png')
-    plt.close()
+    plt.savefig(f"{SAVE_PREFIX}_{FILE_PATH.split('.')[0]}_contrast{CONTRAST_LEVEL}_luminosity_plots.png")
