89fdcce158f689125507c052fe7eb6d3df9a5af7
HidemiMitaniShen <hidemi.mitani1998@gmail.com>
2024-11-21 11:34:23 -0800

    added if statements for the other flags and functionality

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ def main(argv):
     # Parse the arguments
     args = parse_args(argv)
 
-    # Check if the data directory exists
+    ################### Check if the data directory exists (do we want this as a helper function?) ###################
     if not os.path.exists(args.data):
         print(f"Directory {args.data} does not exist")
         sys.exit(1)
@@ -34,7 +34,7 @@ def main(argv):
         sys.exit(1)
     
     
-    # Check if there are any missing files
+    ################### Check if there are any missing files ###################
     missing_files_list = missing_files(args.data, args)
 
     if missing_files_list:
@@ -43,7 +43,38 @@ def main(argv):
     else:
         print("All files are present")
 
+
+    ################### Gif ###################
+    if args.gif:
+        print("Generating gif")
+        # the gif function
+
+    ################### Plots ###################
+    if args.plot_type == 'single':
+        print(f"Plot type: {args.plot_type}")
+        # the single plot function
+    elif args.plot_type == 'multiple':
+        print(f"Plot type: {args.plot_type}")
+        # the multiple plot function
+
+    ################### Line of best fit ###################
+    if args.plot_type == None:
+        print("Please specify plot type")
+        sys.exit(1)
+    elif args.line_of_best_fit:
+        print("Including line of best fit")
+        # the line of best fit function
+
+    ################### Return data for each dish as csv ###################
+    if args.csv:
+        print("Returning data for each dish as csv")
+        # the csv function
     
+
+    
+
+        
+
 
    
 if __name__ == '__main__':
