deca7cfea396a5d8f447cab118acce3867360d8c
Stella <browns63@wwu.edu>
2024-11-21 11:40:53 -0800

    made into a callable function

--- compute_regions.py
+++ compute_regions.py
@@ -7,39 +7,42 @@ VIDEO_PATH = 'video.tif'
 ROI_PATH = 'roi.zip'
 
 
+def compute_regions(video_path = VIDEO_PATH, roi_path = ROI_PATH):
+    rois = np.array(readroi.import_roi(roi_path)).astype(int)
 
-rois = np.array(readroi.import_roi(ROI_PATH)).astype(int)
+    background = rois[-1]
+    coords = rois[:-1]
 
-background = rois[-1]
-coords = rois[:-1]
+    images = np.array(tf.imread(video_path))
+    means = np.zeros(shape=(images.shape[0], coords.shape[0]))
 
-images = np.array(tf.imread(VIDEO_PATH))
-means = np.zeros(shape=(images.shape[0], coords.shape[0]))
-
-background_mean = np.mean(
-    images[
-        :, 
-        background[0]:(background[0] + background[2]),
-        background[1]:(background[1] + background[3])
-    ], axis = (1,2)
-)
-
-
-for i, coord in enumerate(coords):
-    means[:, i] = np.mean(
+    background_mean = np.mean(
         images[
             :, 
-            coord[0]:(coord[0] + coord[2]), 
-            coord[1]:(coord[1] + coord[3])
+            background[0]:(background[0] + background[2]),
+            background[1]:(background[1] + background[3])
         ], axis = (1,2)
     )
 
-    # subtract the background mean from every element
-    means[:, i] -= background_mean
 
-print(means[:20, 0])
+    for i, coord in enumerate(coords):
+        means[:, i] = np.mean(
+            images[
+                :, 
+                coord[0]:(coord[0] + coord[2]), 
+                coord[1]:(coord[1] + coord[3])
+            ], axis = (1,2)
+        )
 
-plt.scatter(range(means.shape[0]), means[:, 0])
-plt.show()
+        # subtract the background mean from every element
+        means[:, i] -= background_mean
 
-np.save('means.npy', means)
+    print(means[:20, 0])
+
+    plt.scatter(range(means.shape[0]), means[:, 0])
+    plt.show()
+
+    return means
+
+if __name__ == '__main__':
+    np.save("means.npy", compute_regions())
